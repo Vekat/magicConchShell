0b02530fc326762bcad2cd6a44a89eda5fbc0051
Vitor Cortez <vitor.a.cortez@gmail.com>
2017-10-31 12:57:27 -0300

    Remove openshift related variables

--- server.js
+++ server.js
@@ -2,56 +2,47 @@
 
 /**
  * @file Creates a Bot object and starts the server.
- * @author Vitor Cortez
  */
 
-var express = require('express'),
+const express = require('express'),
   logger = require('morgan'),
-  parser = require('body-parser');
+  parser = require('body-parser')
 
-var Bot = require('./bot'),
-  routes = require('./routes');
+const Bot = require('./bot'),
+  routes = require('./routes')
 
-var app = express();
+const app = express()
 
-app.set('port', process.env.OPENSHIFT_NODEJS_PORT);
-app.set('host', process.env.OPENSHIFT_NODEJS_IP);
+app.set('port', process.env.PORT || 5000)
 
-const token = process.env.TELEGRAM_API_TOKEN;
-const botURL = `${process.env.OPENSHIFT_APP_DNS}/bot`;
-const apiURL = `${process.env.TELEGRAM_BOT_DNS}/bot${token}`;
+const botURL = `${process.env.HOST}/bot`
+const token = process.env.TELEGRAM_API_TOKEN
+const apiURL = `${process.env.TELEGRAM_API_URL}/bot${token}`
 
-var bot = new Bot(apiURL);
+const bot = new Bot(apiURL)
 
-if (process.env.NODE_ENV == 'debug')
-  app.use(logger('dev'));
+app.use(logger('dev'))
 
-app.get('/', (req, res) => {
-  return res.status(200).send('Bot is up and running!');
-});
+app.get('/', (req, res) => res.status(200).send('bot is up and running'))
 
-app.use(parser.json());
+app.use(parser.json())
 
 // attach bot middlewares
-app.use(routes.init(bot));
+app.use(routes.init(bot))
 
 // handle errors
 app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(err.status || 500).send(err.message);
-});
+  console.error(err.stack)
+  res.status(err.status || 500).send(err.message)
+})
 
-// init bot and start server
+// initialize bot and start server
 bot.init().then(() => {
-  app.listen(app.get('port'), app.get('host'), function() {
-    console.info(`Online: http://${app.get('host')}:${app.get('port')}`);
+  app.listen(app.get('port'), () => {
+    console.info(`listening on port ${app.get('port')}`)
 
     bot.setWebhook(`${botURL}/${bot.id}/${bot.username}`)
-      .then(() => {
-        console.log('webhook set');
-      })
-      .catch(err => {
-        console.error('webhook error', err.message);
-      });
-  });
-});
+      .then(() => { console.info('webhook done') })
+      .catch(err => { console.error('webhook error', err.message) })
+  })
+})
