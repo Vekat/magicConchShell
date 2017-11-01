9711e9776b579b177e49cfff32fae7c30c8abc8d
Vitor Cortez <vitor.a.cortez@gmail.com>
2017-11-01 14:31:51 -0300

    Fix typo

--- middlewares.js
+++ middlewares.js
@@ -82,7 +82,7 @@ const handleHelp = (bot) => {
       "To question the Magic Conch Shell, do not forget to type a question mark `?` in your message.\n",
       "If you are in a group, the Magic Conch will not see any messages by default, so you will have to type `\\ask @magicConchShellBot` in your messages.\n",
       "However, if you give the Magic Conch `admin` rights, she will see all messages, and respond to only those mentioning her (`@magicConchShellBot`).\n",
-      "So, if you do not want the Magic Conch to see all your group messages, remove her `admin` status.\n"
+      "So, if you do not want the Magic Conch to see all your group messages, remove her `admin` status.\n",
       "The answers only make sense if you send a `Yes or No` type of question.\n",
       "Want to rate or review this bot? You can do it here:",
       "telegram.me/storebot?start=magicconchshellbot"
