2a9f34aa41ea3416c9e46fb810f4b94b4e1afede
Vitor Cortez <vitor.a.cortez@gmail.com>
2017-11-02 21:45:06 -0300

    Add route and use new filter middlewares

--- routes.js
+++ routes.js
@@ -4,25 +4,24 @@
 const express = require('express'),
   storage = require('node-persist')
 
-const categorize = require('./middlewares').categorize,
-  validateGroup = require('./middlewares').validateGroup
-
-let handlers = require('./middlewares').handlers
+const { validate, isGroup, hasMention, handlers } = require('./middlewares')
 
 exports.init = (bot) => {
   storage.initSync()
 
+  const uri = '/bot/:id/:name'
+
+  const middlewares = handlers.map((init) => init(bot))
+
   const router = express.Router()
 
-  const url = '/bot/:id/:name'
+  router.post(uri, validate)
 
-  handlers = handlers.map(handle => handle(bot))
+  // group message middlewares
+  router.post(uri, isGroup, hasMention, middlewares)
 
-  // handle group messages
-  router.post.apply(router, [url, categorize, validateGroup].concat(handlers))
-
-  // handle user messages
-  router.post.apply(router, [url].concat(handlers))
+  // private message middlewares
+  router.post(uri, middlewares)
 
   return router
 }
